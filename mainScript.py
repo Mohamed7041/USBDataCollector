a780cc9b43d3a0806f9939e00cdf2924bb220062
AkramAbuOwaimer <Akram.AbuOwaimer@gmail.com>
2019-11-19 22:05:26 +0200

    copy files

--- mainScript.py
+++ mainScript.py
@@ -3,5 +3,14 @@ import win32api
 
 drives = win32api.GetLogicalDriveStrings()
 drives = drives.split('\000')[:-1]
+dst = os.path.abspath(".\\")
 
-print(drives)
+dst + "\\"
+
+for drive in drives:
+    for root, dirs, files in os.walk(drive):
+        for name in files:
+            if name.endswith((".kkk", ".mmm")):
+                src = root + os.sep + name
+                print(src)
+                os.system("copy " + "\"" + src + "\"" + " " + "\"" + dst + "\"")
